2281f85a797cc2528ea2639ff0030429d2421bc2
ruteshdak <ruteshdak@gmail.com>
2024-07-13 15:49:11 +0530

    initial

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{bottom:0,position:'fixed',width:'100%',color:'white',background:'black',padding:'10px'}}>
+      coyright &copy; code with rutesh.com
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='flex justify-between bg-slate-700 text-white py-2'>
+        <div className='logo'>
+          <span className='font-bold text-xl mx-8'>iTask</span>
+        </div>
+        <ul className='flex gap-8 mx-9'>
+            <li className='cursor-pointer hover:font-bold'>Home</li>
+            <li className='cursor-pointer hover:font-bold'>Your task</li>
+            
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
